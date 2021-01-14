8f1592dd517acb1e77c6015ff856a14fdcf8a009
Flavio Samu <fsamu40@gmail.com>
2021-01-14 13:13:23 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/fsamu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/fsamu/workshopmongo/config/Instantiation.java
@@ -10,35 +10,37 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fsamu.workshopmongo.domain.Post;
 import com.fsamu.workshopmongo.domain.User;
+import com.fsamu.workshopmongo.dto.AuthorDTO;
 import com.fsamu.workshopmongo.repository.PostRepository;
 import com.fsamu.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
