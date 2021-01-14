654508a5328f63492f3fe18d64756e75676f3a7e
Flavio Samu <fsamu40@gmail.com>
2021-01-14 14:28:10 -0300

    Consulta simples com query methods

--- src/main/java/com/fsamu/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fsamu/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.fsamu.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.fsamu.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
